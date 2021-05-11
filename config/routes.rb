c4329112688692be935033282fe4bd22bc7f4960
Jordan M. Wofford <wofford.codes@gmail.com>
2021-05-11 19:59:54 -0400

    secured authentication final

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     get '/' => 'users#index'
-    post '/session' => 'sessions#create'
-    post '/users' =>'users#create'
-    get '/dashboard' => 'profiles#index'
+    # post '/session' => 'sessions#create'
+    # post '/users' =>'users#create'
+    # get '/dashboard' => 'profiles#index'
 
 end
